29b61a7c1347d5d19260c0b8567009cf6f0320bd
sunghwan <tjdghks130907@gmail.com>
2025-04-14 22:35:16 +0900

    좋아요 수 구현 - 비관적 락(update문, select...for update), 낙관적 락(version)

--- service/like/src/main/java/kuke/board/like/service/ArticleLikeService.java
+++ service/like/src/main/java/kuke/board/like/service/ArticleLikeService.java
@@ -2,6 +2,8 @@ package kuke.board.like.service;
 
 import kuke.board.common.snowflake.Snowflake;
 import kuke.board.like.entity.ArticleLike;
+import kuke.board.like.entity.ArticleLikeCount;
+import kuke.board.like.repository.ArticleLikeCountRepository;
 import kuke.board.like.repository.ArticleLikeRepository;
 import kuke.board.like.service.response.ArticleLikeResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ArticleLikeService {
     private final Snowflake snowflake = new Snowflake();
     private final ArticleLikeRepository articleLikeRepository;
+    private final ArticleLikeCountRepository articleLikeCountRepository;
 
     public ArticleLikeResponse read(Long articleId, Long userId) {
         return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
@@ -20,8 +23,12 @@ public class ArticleLikeService {
                 .orElseThrow();
     }
 
+    /**
+     * 비관적 락 - update 구문
+     * 락을 점유하는 시간이 상대적으로 짧다 ( update 문을 수행하는 시점에 락을 점유 )
+     */
     @Transactional
-    public void like(Long articleId, Long userId) {
+    public void likePessimisticLock1(Long articleId, Long userId) {
         articleLikeRepository.save(
                 ArticleLike.create(
                         snowflake.nextId(),
@@ -29,11 +36,90 @@ public class ArticleLikeService {
                         userId
                 )
         );
+
+        int result = articleLikeCountRepository.increase(articleId);
+        if (result == 0) {
+            // 최초 요청 시에는 update 되는 레코드가 없으므로, 1로 초기화한다.
+            // 트래픽이 순식간에 몰릴 수 있는 상황에는 유실될 수 있으므로, 게시글 생성 시점에 미리 0으로 초기화 해두는 전략을 활용할 수 있다.
+            articleLikeCountRepository.save(
+                    ArticleLikeCount.init(articleId, 1L)
+            );
+        }
     }
 
     @Transactional
-    public void unlike(Long articleId, Long userId) {
+    public void unlikePessimisticLock1(Long articleId, Long userId) {
         articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
-                .ifPresent(articleLikeRepository::delete);
+                .ifPresent(articleLike -> {
+                    articleLikeRepository.delete(articleLike);
+                    articleLikeCountRepository.decrease(articleId);
+                });
     }
+
+    /**
+     * 비관적 . - select ... for update + update
+     * 락을 점유하는 시간이 상대적으로 길다.
+     * 데이터를 조회한 뒤 중간 과정을 수행해야 하기 때문에, 락 해제가 지연될 수 있다.
+     */
+    @Transactional
+    public void likePessimisticLock2(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+
+        ArticleLikeCount articleLikeCount = articleLikeCountRepository.findLockedByArticleId(articleId)
+                .orElseGet(() -> ArticleLikeCount.init(articleId, 0L));
+        articleLikeCount.increase();
+        articleLikeCountRepository.save(articleLikeCount);
+    }
+
+    @Transactional
+    public void unlikePessimisticLock2(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(entity -> {
+                    articleLikeRepository.delete(entity);
+                    ArticleLikeCount articleLikeCount = articleLikeCountRepository.findLockedByArticleId(articleId).orElseThrow();
+                    articleLikeCount.decrease();
+                });
+    }
+
+    /**
+     * 낙관적 락은 실패 시 재시도나 예외에 대한 처리를 직접 구현해야한다.
+     */
+    @Transactional
+    public void likeOptimisticLock(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+
+        ArticleLikeCount articleLikeCount = articleLikeCountRepository.findById(articleId)
+                .orElseGet(() -> ArticleLikeCount.init(articleId, 0L));
+        articleLikeCount.increase();
+        articleLikeCountRepository.save(articleLikeCount);
+    }
+
+    @Transactional
+    public void unlikeOptimisticLock(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(entity -> {
+                    articleLikeRepository.delete(entity);
+                    ArticleLikeCount articleLikeCount = articleLikeCountRepository.findById(articleId).orElseThrow();
+                    articleLikeCount.decrease();
+                });
+    }
+
+    public Long count(Long articleId) {
+        return articleLikeCountRepository.findById(articleId)
+                .map(ArticleLikeCount::getLikeCount)
+                .orElse(0L);
+    }
+
 }
