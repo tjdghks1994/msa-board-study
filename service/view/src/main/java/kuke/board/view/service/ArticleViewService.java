5fc188e3e970d4c241e99952a3cc503f12c7c350
sunghwan <tjdghks130907@gmail.com>
2025-04-16 22:32:20 +0900

    조회수 어뷰징 방지 설계 - 사용자별 10분당 1회만 증가 ( Redis의 분산 락, TTL 활용 )

--- service/view/src/main/java/kuke/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/kuke/board/view/service/ArticleViewService.java
@@ -1,17 +1,28 @@
 package kuke.board.view.service;
 
 import kuke.board.view.repository.ArticleViewCountRepository;
+import kuke.board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
     private static final int BACK_UP_BATCH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10); // 10분
 
     public Long increase(Long articleId, Long userId) {
+        // 분산 락 획득을 시도하고, 획득에 실패한 경우 현재 조회수를 그대로 반환 처리 ( 조회수는 사용자별 10분당 1씩만 증가하도록 설계 )
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if(count % BACK_UP_BATCH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);
